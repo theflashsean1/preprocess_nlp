c7da89cf4c3567765ce39cbc618b4731a1f2550f
sheng <sheng.jia@mail.utoronto.ca>
2017-12-22 21:31:42 -0800

    tf vocab class added

--- test_corpus.py
+++ test_corpus.py
@@ -13,5 +13,6 @@ with VocabCreator(100, "vocab.txt", "vocab_counts.txt") as vocab_creator:
 
 #doc.save_seq("rnn_lang_model", "rnn_src.txt", "rnn_tgt.txt",
 #             batch_size=64, seq_len=15, nb_epochs=2)
-doc.save_seq("word2vec", "word2vec_src.txt", "word2vec_tgt.txt",
-             window_size=2, max_num_examples=1000)
+
+#doc.save_seq("word2vec", "word2vec_src.txt", "word2vec_tgt.txt",
+#             window_size=2, max_num_examples=1000)
--- vocab_utils/tf_vocab.py
+++ vocab_utils/tf_vocab.py
@@ -1,21 +1,46 @@
 import tensorflow as tf 
+from tensorflow.python.ops import lookup_ops
+from vocab_utils.common import UNK_ID, UNK
 
 class VocabReader(object):
     def __init__(self, vocab_f_path, count_f_path=None):
-        pass
+        self._vocab_f_path = vocab_f_path
+        self._count_f_path = count_f_path
+        self._vocab_size = None
+        self._vocab_counts = None
+        self._word2id_table = None
+        self._id2word_table = None
 
-    def tf_ids_lookup_op(self, str_tensor):
-        return tf.cast(self._word2id_table.lookup(str_tensor), tf.int32)
+    def id2word_lookup(self, id_token):
+        if not self._id2word_table:
+            with tf.variable_scope("vocab_lookup"):
+                self._id2word_table = lookup_ops.index_to_string_table_from_file(
+                    vocab_f_path, default_value=UNK, name="id2word"
+                )
+        return self._id2word_table
 
-    def tf_words_lookup_op(self, id_tensor):
-        return self._id2word_table.lookup(tf.to_int64(id_tensor))
+    def word2id_lookup(self, word_token):
+        if not self._word2id_table:
+            with tf.variable_scope("vocab_lookup"):
+                self._word2id_table = lookup_ops.index_table_from_file(
+                    vocab_f_path, default_value=UNK_ID, name="word2id"
+                )
+        return self._word2id_table
 
     @property
     def vocab_size(self):
+        if not self._vocab_size:
+            with open(self._vocab_f_path) as f:
+                self._vocab_size = len(f.read().strip().split())
         return self._vocab_size
 
     @property
     def vocab_counts_list(self):
+        if not self._vocab_counts and count_f_path:
+            with open(count_f_path) as count_f:
+                counts = count_f.read().strip().split()
+                self._vocab_counts = [int(count) for count in counts]
         return self._vocab_counts
+
 
 
